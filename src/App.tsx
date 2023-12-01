1fd5440df6a899284f9a5e88603811bd0d012159
Mostakimw <scientist.expert@gmail.com>
2023-12-01 16:06:09 +0600

    basic setup and tailwind installation with ts project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/shared/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-red-400">hello</h1>
+    </div>
+  );
+};
+
+export default App;
