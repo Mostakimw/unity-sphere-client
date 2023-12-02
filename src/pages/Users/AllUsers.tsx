ce9fefc49321447c3d890fc796256101be86049f
Mostakimw <scientist.expert@gmail.com>
2023-12-02 20:42:36 +0600

    search implemented

--- src/pages/Users/AllUsers.tsx
+++ src/pages/Users/AllUsers.tsx
@@ -8,18 +8,20 @@ const AllUsers = () => {
   const [users, setUsers] = useState<TUser[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [totalPages, setTotalPages] = useState<number>(1);
+  const [searchQuery, setSearchQuery] = useState<string>("");
   const PAGE_SIZE = 20;
 
   // fetching users by useEffect
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await fetch(
-          `http://localhost:5000/api/users?page=${currentPage}&pageSize=${PAGE_SIZE}`
-        );
+        const apiUrl = `http://localhost:5000/api/users?page=${currentPage}&pageSize=${PAGE_SIZE}${
+          searchQuery ? `&query=${searchQuery}` : ""
+        }`;
+
+        const response = await fetch(apiUrl);
         const { data } = await response.json();
         const { users, totalPages } = data;
-        console.log(data);
 
         setUsers(users);
         setTotalPages(totalPages || 1);
@@ -29,10 +31,19 @@ const AllUsers = () => {
     };
 
     fetchUsers();
-  }, [currentPage]);
+  }, [currentPage, searchQuery]);
 
   const handlePageChange = (newPage: number) => {
     setCurrentPage(newPage);
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearchSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setCurrentPage(1); // Reset page when searching
   };
 
   return (
@@ -40,47 +51,68 @@ const AllUsers = () => {
       <Container>
         <div className="mt-12">
           <SectionTitle title="ALL USERS" />
+
+          {/* Search form */}
+          <form onSubmit={handleSearchSubmit} className="mb-4">
+            <input
+              type="text"
+              placeholder="Search by name..."
+              value={searchQuery}
+              onChange={handleSearchChange}
+              className="border p-2"
+            />
+            <button type="submit" className="bg-blue-500 text-white p-2 ml-2">
+              Search
+            </button>
+          </form>
+
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-            {users.map((user) => (
-              <UserCard key={user.id} user={user} />
-            ))}
+            {users.length === 0 ? (
+              <p>No users found</p>
+            ) : (
+              users.map((user) => <UserCard key={user.id} user={user} />)
+            )}
           </div>
 
-          {/* pagination btn here  */}
-          <div className="flex justify-center my-12">
-            {Array.from({ length: totalPages }, (_, index) => {
-              const maxVisibleButtons = 5;
-              const start = Math.max(
-                0,
-                currentPage - Math.floor(maxVisibleButtons / 2)
-              );
-              const end = Math.min(totalPages, start + maxVisibleButtons);
+          {/* Pagination buttons */}
+          {users.length === 0 ? (
+            <p></p>
+          ) : (
+            <div className="flex justify-center my-12">
+              {Array.from({ length: totalPages }, (_, index) => {
+                const maxVisibleButtons = 5;
+                const start = Math.max(
+                  0,
+                  currentPage - Math.floor(maxVisibleButtons / 2)
+                );
+                const end = Math.min(totalPages, start + maxVisibleButtons);
 
-              const showEllipsisStart = start > 0;
-              const showEllipsisEnd = end < totalPages;
+                const showEllipsisStart = start > 0;
+                const showEllipsisEnd = end < totalPages;
 
-              return (
-                <React.Fragment key={index + 1}>
-                  {showEllipsisStart && index === 0 && <span>...</span>}
-                  {index >= start && index < end && (
-                    <button
-                      className={`mx-2 p-2 ${
-                        currentPage === index + 1
-                          ? "bg-blue-500 text-white"
-                          : ""
-                      }`}
-                      onClick={() => handlePageChange(index + 1)}
-                    >
-                      {index + 1}
-                    </button>
-                  )}
-                  {showEllipsisEnd && index === totalPages - 1 && (
-                    <span>...</span>
-                  )}
-                </React.Fragment>
-              );
-            })}
-          </div>
+                return (
+                  <React.Fragment key={index + 1}>
+                    {showEllipsisStart && index === 0 && <span>...</span>}
+                    {index >= start && index < end && (
+                      <button
+                        className={`mx-2 p-2 ${
+                          currentPage === index + 1
+                            ? "bg-blue-500 text-white"
+                            : ""
+                        }`}
+                        onClick={() => handlePageChange(index + 1)}
+                      >
+                        {index + 1}
+                      </button>
+                    )}
+                    {showEllipsisEnd && index === totalPages - 1 && (
+                      <span>...</span>
+                    )}
+                  </React.Fragment>
+                );
+              })}
+            </div>
+          )}
         </div>
       </Container>
     </>
